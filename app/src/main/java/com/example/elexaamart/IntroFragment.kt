e1f0b79f345587ee7a78c696b62fb105b1e0c7f0
website to android app <sakibmina83@gmail.com>
2025-02-21 16:53:51 +0600

    initial commit

---
+++ app/src/main/java/com/example/elexaamart/IntroFragment.kt
@@ -0,0 +1,34 @@
+package com.example.elexaamart
+
+
+import android.os.Bundle
+import androidx.fragment.app.Fragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import com.example.elexaamart.databinding.FragmentIntroBinding
+
+class IntroFragment : Fragment() {
+
+    private lateinit var binding: FragmentIntroBinding
+
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        binding = FragmentIntroBinding.inflate(inflater, container, false)
+        starter()
+        return binding.root
+    }
+
+    private fun starter() {
+        binding.loginBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_introFragment_to_loginFragment)
+        }
+        binding.registerBtn.setOnClickListener {
+            findNavController().navigate(R.id.action_introFragment_to_registerFragment)
+        }
+    }
+}
+
